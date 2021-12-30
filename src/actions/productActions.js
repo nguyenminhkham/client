7f68636835d29e9216d78951c9f889cccb0acf21
unknown <ng.minhkham@gmail.com>
2021-12-30 07:54:23 +0700

    commit

--- src/actions/productActions.js
+++ src/actions/productActions.js
@@ -13,7 +13,7 @@ export const listProducts = () => async (dispatch) => {
         type: PRODUCT_LIST_REQUEST,
     })
     try {
-        const { data } = await Axios.get('/api/products')
+        const { data } = await Axios.get('https://banvecokhi.com/api/products')
         dispatch({type: PRODUCT_LIST_SUCCESS, payload: data})
     } catch (error) {
         dispatch({type: PRODUCT_LIST_FAIL, payload: error.message})
@@ -24,7 +24,7 @@ export const detailsProduct = (productId) => async (dispatch) => {
         dispatch({
             type: PRODUCT_DETAILS_REQUEST, payload: productId})
         try {
-            const { data } = await Axios.get(`/api/products/${productId}`)
+            const { data } = await Axios.get(`https://banvecokhi.com/api/products/${productId}`)
             dispatch({ type: PRODUCT_DETAILS_SUCCESS, payload: data})
         } catch (error) {
             dispatch({
